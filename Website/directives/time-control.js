058de9b5fc971e65dc4a5616ea21791916500baf
Ryan Musgrave <ryanm128@gmail.com>
2015-07-04 15:17:40 +0800

    Improved look of "Next" button.

---
+++ Website/directives/time-control.js
@@ -0,0 +1,10 @@
+angular.module('synergyCity')
+  .directive('timeControl', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'directives/time-control.html',
+      controller: function() {
+        
+      }
+    };
+  });
